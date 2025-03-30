6f0fe6d791dc4b3d191ac251427df40686ef9dad
mrjvs <mistrjvs@gmail.com>
2025-03-30 22:25:17 +0200

    Implement cli

--- apps/api/src/cli.ts
+++ apps/api/src/cli.ts
@@ -1,54 +1,78 @@
 import { Command, program } from 'commander';
-import { version } from './config';
+import { conf, version } from './config';
 import { logDivide, logger, logIntro } from './modules/log';
 import execSh from 'exec-sh';
-import { cancel, isCancel, log, text } from '@clack/prompts';
+import { cancel, confirm, isCancel, log, spinner, text } from '@clack/prompts';
+import { prisma } from './modules/db';
+import { appRoles } from './utils/permissions/roles';
+
+async function migrate() {
+  try {
+    await execSh.promise('node node_modules/prisma/build/index.js migrate deploy', {
+      env: {
+        ...process.env,
+        STL_DB__CONNECTION: conf.db.connection,
+      },
+    });
+  } catch (err: any) {
+    console.error(err.message);
+  }
+  console.log('');
+}
 
 const initCmd = new Command('init')
   .description('Initialize Statulo')
   .action(async () => {
     logIntro(true);
     logger.info('Initialising database...');
-    await execSh.promise('node node_modules/drizzle-kit/bin.cjs migrate');
-    console.log('');
+    await migrate();
     logDivide();
     logger.info('Initialisation complete!');
   });
 
 const promoteCmd = new Command('promote')
-  .description('Promote a user to Super')
+  .description('Promote a user to Admin')
   .action(async () => {
     logIntro(true);
-    const username = await text({
-      message: 'What user do you want to promote to Super?',
-      placeholder: 'my-cool-username',
+    const email = await text({
+      message: 'What user do you want to promote to Admin?',
+      placeholder: 'john@example.com',
     });
-    if (isCancel(username)) {
+    if (isCancel(email)) {
       cancel('Operation cancelled.');
       process.exit(1);
     }
 
-    // const [user] = await db.select().from(users).where(eq(users.username, username));
-    // if (!user) {
-    //   log.error('Could not find user with that username!');
-    //   process.exit(1);
-    // }
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      log.error('Could not find user with that username!');
+      process.exit(1);
+    }
 
-    // const shouldContinue = await confirm({
-    //   message: `You are about to promote '${username}' to Super. Want to proceed?`,
-    //   initialValue: false,
-    // });
-    // if (isCancel(shouldContinue)) {
-    //   cancel('Operation cancelled.');
-    //   process.exit(1);
-    // }
+    const shouldContinue = await confirm({
+      message: `You are about to promote '${email}' to Super. Want to proceed?`,
+      initialValue: false,
+    });
+    if (isCancel(shouldContinue)) {
+      cancel('Operation cancelled.');
+      process.exit(1);
+    }
 
-    // const s = spinner();
-    // s.start('Promoting user');
-    // await db.update(users).set({
-    //   roles: [...(user.roles ?? []), roles.super],
-    // }).where(eq(users.id, user.id));
-    // s.stop('Promoted user!');
+    const s = spinner();
+    s.start('Promoting user');
+    await prisma.user.update({
+      where: {
+        email,
+      },
+      data: {
+        roles: [...(user.roles ?? []), appRoles.admin],
+      },
+    });
+    s.stop('Promoted user!');
 
     log.success('User has successfully been promoted!');
     process.exit(0);
@@ -59,8 +83,7 @@ const migrateCmd = new Command('migrate')
   .action(async () => {
     logIntro(true);
     logger.info('Running migrations...');
-    await execSh.promise('node node_modules/drizzle-kit/bin.cjs migrate');
-    console.log('');
+    await migrate();
     logDivide();
     logger.info('Migrations completed!');
   });
