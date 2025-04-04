6e3619502233e8491c15561e0af217556312189c
mrjvs <mistrjvs@gmail.com>
2025-04-04 20:35:34 +0200

    Rename permission variables

--- apps/api/src/utils/permissions/role-permissions.ts
+++ apps/api/src/utils/permissions/role-permissions.ts
@@ -6,10 +6,10 @@ export const basePerms: Permission[] = [
   permissions.user.create({}),
 ];
 
-export const baseUserPerms = (user: string): Permission[] => [
+export const baseUserPerms = (userId: string): Permission[] => [
   permissions.org.create({}),
-  permissions.user.read({ usr: user }),
-  permissions.user.delete({ usr: user }),
+  permissions.user.read({ usr: userId }),
+  permissions.user.delete({ usr: userId }),
 ];
 
 export const rolePerms: Record<AppRoles, Permission[]> = {
@@ -21,19 +21,19 @@ export const rolePerms: Record<AppRoles, Permission[]> = {
   ],
 };
 
-export const orgRolePerms: Record<OrgRoles, (org: string) => Permission[]> = {
-  'org:viewer': org => [
-    permissions.org.read({ org }),
-    permissions.org.member.read({ org, mbr: all }),
-    permissions.org.member.list({ org }),
+export const orgRolePerms: Record<OrgRoles, (orgId: string) => Permission[]> = {
+  'org:viewer': orgId => [
+    permissions.org.read({ org: orgId }),
+    permissions.org.member.read({ org: orgId, mbr: all }),
+    permissions.org.member.list({ org: orgId }),
   ],
-  'org:admin': org => [
-    ...orgRolePerms['org:viewer'](org),
+  'org:admin': orgId => [
+    ...orgRolePerms['org:viewer'](orgId),
 
-    permissions.org.delete({ org }),
-    permissions.org.edit({ org }),
-    permissions.org.member.create({ org }),
-    permissions.org.member.edit({ org, mbr: all }),
-    permissions.org.member.delete({ org, mbr: all }),
+    permissions.org.delete({ org: orgId }),
+    permissions.org.edit({ org: orgId }),
+    permissions.org.member.create({ org: orgId }),
+    permissions.org.member.edit({ org: orgId, mbr: all }),
+    permissions.org.member.delete({ org: orgId, mbr: all }),
   ],
 };
