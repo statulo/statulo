0f10bb1d529bc8db1a7f371ba809b8a29e8e02a1
William Oldham <github@binaryoverload.co.uk>
2025-04-13 15:03:43 +0100

    Remove email verification check from org creation

--- apps/api/src/routes/v1/orgs.ts
+++ apps/api/src/routes/v1/orgs.ts
@@ -24,8 +24,8 @@ export const orgRouter = makeRouter((app) => {
     },
     handle(async ({ body, auth }) => {
       auth.checkAuthentication();
-      auth.checkEmailVerified();
       auth.can(permissions.org.create({}));
+
       const newOrg = await prisma.organisation.create({
         data: {
           id: getId('org'),
@@ -40,6 +40,7 @@ export const orgRouter = makeRouter((app) => {
           },
         },
       });
+
       return mapOrganisation(newOrg);
     }),
   );
