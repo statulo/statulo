ed6ba7b451fea418d301aa18259ef44a4f19e0ee
William Oldham <github@binaryoverload.co.uk>
2025-04-15 20:37:47 +0100

    Flesh out Nuxt project with libraries

--- apps/dashboard/nuxt.config.ts
+++ apps/dashboard/nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+
+  future: {
+    compatibilityVersion: 4
+  },
+
   devtools: { enabled: true },
 
   modules: [
@@ -8,6 +13,11 @@ export default defineNuxtConfig({
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
-    '@nuxt/scripts'
+    '@nuxt/scripts',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate/nuxt',
+    '@nuxtjs/robots'
   ]
 })
