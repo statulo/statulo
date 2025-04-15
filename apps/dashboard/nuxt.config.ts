a3e786f0016774f22e01382ce4859d4e155ac415
William Oldham <github@binaryoverload.co.uk>
2025-04-15 20:48:03 +0100

    Use latest compatibility version

--- apps/dashboard/nuxt.config.ts
+++ apps/dashboard/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2025-04-15',
 
   future: {
     compatibilityVersion: 4
