bbe46143d8a8370ee3b0569884a30453b8fed3b6
William Oldham <github@binaryoverload.co.uk>
2025-04-18 18:49:06 +0100

    Fix linting for status page file

--- apps/api/src/routes/v1/status-pages.ts
+++ apps/api/src/routes/v1/status-pages.ts
@@ -1,12 +1,12 @@
+import { z } from "zod";
+import type { Prisma } from "@prisma/client";
 import { prisma } from "@/modules/db";
 import { handle } from "@/utils/handle";
 import { getExternalId, getId } from "@/utils/id";
 import { permissions } from "@/utils/permissions/permissions";
 import { makeRouter } from "@/utils/router";
-import { z } from "zod";
 import { NotFoundError } from "@/utils/error";
 import { mapPage, pagerSchema } from "@/utils/pages";
-import type { Prisma } from "@prisma/client";
 import { mapStatusPage } from "@/routes/v1/mappings/status-page";
 
 export const statusPageRouter = makeRouter((app) => {
