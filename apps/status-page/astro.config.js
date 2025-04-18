b8b84c130fb815135633bbdd22cf2ca154e2d799
William Oldham <github@binaryoverload.co.uk>
2025-04-18 14:25:23 +0100

    Add astro icon integration

--- apps/status-page/astro.config.js
+++ apps/status-page/astro.config.js
@@ -1,14 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
 
   adapter: node({
     mode: "standalone",
   }),
+
+  env: {
+    schema: {
+
+    },
+  },
 });
