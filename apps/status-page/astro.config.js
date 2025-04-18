e0dbe514f97dbbdb6bbef3089cb688586aebafe4
William Oldham <github@binaryoverload.co.uk>
2025-04-18 14:23:17 +0100

    Add node adapter to astro

--- apps/status-page/astro.config.js
+++ apps/status-page/astro.config.js
@@ -2,7 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
