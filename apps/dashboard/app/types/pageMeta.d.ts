adab75f3cee04b92e955c75806725f21a05e1921
William Oldham <github@binaryoverload.co.uk>
2025-05-03 15:05:52 +0100

    Use proper type for page meta augmentation

--- apps/dashboard/app/types/pageMeta.d.ts
+++ apps/dashboard/app/types/pageMeta.d.ts
@@ -1,7 +1,8 @@
-import "nuxt/dist/pages/runtime/composables";
-
-declare module "nuxt/dist/pages/runtime/composables" {
+declare module "#app" {
   interface PageMeta {
     auth?: boolean | "guest";
   }
 }
+
+// It is always important to ensure you import/export something when augmenting a type
+export {};
