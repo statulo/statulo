52ae49606cc15b8f1835ef3f5db574d4ad8ce6a2
William Oldham <github@binaryoverload.co.uk>
2025-06-14 15:00:06 +0100

    Add change detection to form composable

---
+++ apps/dashboard/app/utils/objects.ts
@@ -0,0 +1,26 @@
+import isEqual from "lodash/isEqual";
+
+export function getDeepChangedFields<T>(a: T, b: T): string[] {
+  const changes: string[] = [];
+
+  console.log("Comparing objects:", a, b);
+
+  const compare = (objA: any, objB: any, path: string[] = []) => {
+    if (typeof objA !== "object" && typeof objB !== "object" && isEqual(objA, objB)) return;
+
+    if (typeof objA !== "object" || typeof objB !== "object" || objA === null || objB === null) {
+      changes.push(path.join("."));
+      return;
+    }
+
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+
+    for (const key of new Set([...keysA, ...keysB])) {
+      compare(objA[key], objB[key], [...path, key]);
+    }
+  };
+
+  compare(a, b);
+  return changes;
+}
