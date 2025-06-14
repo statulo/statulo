dea2dd25b39b8687f5a9494ae71f725de2d676bc
William Oldham <github@binaryoverload.co.uk>
2025-06-14 10:58:01 +0100

    Add pin input

---
+++ apps/dashboard/app/components/inputs/PinInput.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { PinInputInput, PinInputRoot } from "reka-ui";
+
+const props = defineProps<{
+  disabled?: boolean;
+  mask?: boolean;
+  otp?: boolean;
+  required?: boolean;
+  type?: "text" | "number";
+  count: number;
+}>();
+
+const type = computed(() => {
+  return props.type ?? "number";
+});
+
+const value = defineModel<number[] | string[]>();
+</script>
+
+<template>
+  <PinInputRoot
+    v-model="value"
+    :disabled="props.disabled"
+    :mask="props.mask"
+    :otp="props.otp"
+    :required="props.required"
+    :type="type"
+    placeholder="0"
+    class="flex items-center gap-2"
+  >
+    <PinInputInput
+      v-for="(id, index) in props.count"
+      :key="id"
+      :index="index"
+      class="w-10 h-10 text-center rounded-lg outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 focus:ring-offset-neutral-500 bg-neutral-600 text-neutral-100 placeholder:text-neutral-400 disabled:bg-neutral-500 disabled:cursor-not-allowed"
+    />
+  </PinInputRoot>
+</template>
