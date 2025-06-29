9d10d3b9a6227aa20a11ca51fef54bbee0dd1bf6
William Oldham <github@binaryoverload.co.uk>
2025-06-29 14:00:53 +0100

    Move health check to /healthz

--- apps/api/src/routes/v0/index.ts
+++ apps/api/src/routes/v0/index.ts
@@ -19,18 +19,34 @@ async function healthcheck(): Promise<Check[]> {
 
 export const indexRouter = makeRouter((app) => {
   app.get(
-    "",
+    "/",
     {
       schema: {
-        description: "Healthcheck",
+        description: "Index route",
+      },
+    },
+    handle(async ({ res }) => {
+      void res.status(404);
+      return {
+        message: "Welcome to Statulo API! Please check the documentation for more information.",
+        version,
+      };
+    }),
+  );
+  app.get(
+    "/healthz",
+    {
+      schema: {
+        description: "Health check",
       },
     },
     handle(async ({ res }) => {
       const checks = await healthcheck();
       const isHealthy = checks.every(v => v.success);
-      void res.status(isHealthy ? 200 : 500);
+      void res.status(isHealthy ? 200 : 503);
       return {
-        message: "API server is working!",
+        message: isHealthy ? "API is healthy" : "API is unhealthy",
+        isHealthy,
         version,
         checks,
       };
