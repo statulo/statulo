73ec2b5c35ada7e0946d8c3679d97c5de314f1a7
mrjvs <jellevs@gmail.com>
2025-07-09 10:31:37 +0200

    Force shutdown on second interrupt signal

--- apps/scout/cmd/scout/signal.go
+++ apps/scout/cmd/scout/signal.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"context"
+	"os"
 	"os/signal"
-	"syscall"
 )
 
 func listenSignals(cancel context.CancelFunc) {
-	notifyCtx, _ := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	notifyCtx, _ := signal.NotifyContext(context.Background(), os.Interrupt)
 
 	go func() {
 		<-notifyCtx.Done()
-		log.Info("Shutdown signal received")
+		log.Info("Shutdown signal received, gracefully shutting down. Send INTERRUPT again to forcibly shut down and risk missing checks")
 		cancel()
+
+		// Second interrupt causes full shut down
+		secondNotifyCtx, _ := signal.NotifyContext(context.Background(), os.Interrupt)
+		<-secondNotifyCtx.Done()
+		log.Info("Second shutdown signal received, forcibly shutting down")
+		os.Exit(1)
 	}()
 }
