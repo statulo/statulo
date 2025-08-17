6d7db2f1edbe376acc9a1c431b35d333db9fa5ea
mrjvs <jellevs@gmail.com>
2025-08-17 18:31:59 +0200

    Switch cron library

--- apps/api/src/modules/scheduled/index.ts
+++ apps/api/src/modules/scheduled/index.ts
@@ -1,4 +1,4 @@
-import cron from "node-cron";
+import { CronJob } from "cron";
 import { logger } from "@/modules/log";
 import { executeCleanupPendingEmailVerifications } from "@/modules/scheduled/cleanup";
 import { executeOrchestratorHeartbeat } from "@/modules/scheduled/heartbeat";
@@ -6,13 +6,17 @@ import { executeOrchestratorHeartbeat } from "@/modules/scheduled/heartbeat";
 const log = logger.child({ svc: "scheduler" });
 
 function registerSchedule(schedule: string, name: string, fn: () => void | Promise<void>) {
-  cron.schedule(schedule, async () => {
-    try {
-      const result = fn();
-      await result;
-    } catch (err) {
-      log.error(`Error in schedule ${name}`, err);
-    }
+  CronJob.from({
+    cronTime: schedule,
+    onTick: async () => {
+      try {
+        const result = fn();
+        await result;
+      } catch (err) {
+        log.error(`Error in schedule ${name}`, err);
+      }
+    },
+    start: true,
   });
   log.info(`Added schedule ${name} for ${schedule}`);
 }
